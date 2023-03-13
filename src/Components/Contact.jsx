b534b3ce9b85a51d4749267c3c58a4f3752acc5e
= <=>
2023-03-13 19:11:11 +0530

    7

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+
+
+
+<br/>
+<br/>
+<hr/>
+        For More Information Visit Our Site : www.ashokartsdeora.com
+        <hr/>   
+        
+    </div>
+  )
+}
+
+export default Contact
